58aa1804c6e99a69479e61e318ed96e0d99fed4a
tejas-patil-infy <tejasashok.patil@infosys.com>
2025-06-22 14:57:40 +0530

    Added SideNav

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   // This is the officially supported way in Next.js 15.3.4
   experimental: {
     webpackBuildWorker: true // Forces Webpack instead of Turbopack
-  }
+  },
+  // devIndicators: false
 };
 
 export default nextConfig;
